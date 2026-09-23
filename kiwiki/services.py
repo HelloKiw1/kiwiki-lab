1048f8d750053c6023848466935f5902563c0004
Eduardo Henrique <eduardohenriquerramos@gmail.com>
2026-09-23 11:17:45 -0300

    Remove unused services

--- kiwiki/services.py
+++ kiwiki/services.py
@@ -24,10 +24,8 @@ SERVICES = (
     ServiceConfig(id="kiwiki", name="Kiwiki Lab", ports=(5000,)),
     ServiceConfig(id="codex", name="Django / Codex", ports=(8000,)),
     ServiceConfig(id="nginx", name="Nginx", ports=(8080, 8081)),
-ServiceConfig(id="ssh", name="SSH", ports=(8022,)),
+    ServiceConfig(id="ssh", name="SSH", ports=(8022,)),
     ServiceConfig(id="postgresql", name="PostgreSQL", ports=(5432,)),
-    ServiceConfig(id="mysql", name="MySQL / MariaDB", ports=(3306,)),
-    ServiceConfig(id="redis", name="Redis", ports=(6379,)),
 )
 
 
